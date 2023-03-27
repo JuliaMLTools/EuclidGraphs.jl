e556a505ce3ac2f24651ab1ab5122804f65ddd55
JuliaMLTools <JuliaMLTools@gmail.com>
2023-03-27 09:47:57 -0400

    ngon fn

---
+++ src/ngon.jl
@@ -0,0 +1,17 @@
+"""
+    Get the vertices of a regular polygon.
+"""
+function ngon(n::Int, r::Real=80)
+    θ = 2π/n
+    tround(xy) = round.(xy, digits=2)
+    [(r*cos(i*θ), r*sin(i*θ)) for i in 0:n-1] .|> tround
+end
+
+triangle(args...) = ngon(3, args...)
+square(args...) = ngon(4, args...)
+pentagon(args...) = ngon(5, args...)
+hexagon(args...) = ngon(6, args...)
+heptagon(args...) = ngon(7, args...)
+octagon(args...) = ngon(8, args...)
+nonagon(args...) = ngon(9, args...)
+decagon(args...) = ngon(10, args...)
